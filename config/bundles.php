a0b9b0e036343e084f7d5e5339527f09b0590865
Franco Pizzutti <fpizzuttimed@gmail.com>
2023-11-16 10:06:00 -0300

    Adds maker bundle as prod dependency to tackle microkernel issue

--- config/bundles.php
+++ config/bundles.php
@@ -3,8 +3,8 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
