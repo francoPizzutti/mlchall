8d9edc7575834fcbb921adf6282f7ac311a96038
Franco Pizzutti <fpizzuttimed@gmail.com>
2023-11-16 12:53:01 -0300

    test cache items not working as expected

--- src/Controller/SpaceshipController.php
+++ src/Controller/SpaceshipController.php
@@ -91,7 +91,7 @@ class SpaceshipController extends AbstractFOSRestController {
         $sato = $this->cacheService->getItem('sato');
 
         $cacheItems = [$kenobi, $skywalker, $sato];
-        
+        var_dump($cacheItems);
         if(!$this->cacheService->validateAllItemsHit($cacheItems)) {
             return new JsonResponse([
                 'Not enough information to retrieve Spaceship data yet.',
